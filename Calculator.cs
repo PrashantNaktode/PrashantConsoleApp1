4cd8f660df134ea0c50df704bd4f0c6eceaa0f77
PrashantNaktode <100767502+PrashantNaktode@users.noreply.github.com>
2022-03-02 22:17:17 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logics1
+{
+    public class Calculator
+    {
+        
+        public void Addition()
+        {
+            int a = 50;
+            int b = 20;
+            int res = a + b;
+            Console.WriteLine("The Addition Of a and b is" + res);
+        }
+        public void Subtraction()
+        {
+            int a = 50;
+            int b = 20;
+            int res = a - b;
+            Console.WriteLine("The Subtraction of a and b is", res);
+        }
+    }
+}
