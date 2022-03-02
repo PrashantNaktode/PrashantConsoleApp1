a035631706531b7722cc14fdf3892468963d38cb
krutikanaktode31 <krutikanaktode123@gmail.com>
2022-03-02 16:52:19 +0530

    Add project files.

---
+++ Inheritence.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrashantConsoleApp1
+{
+    class Inheritence
+    {
+        public class user  //parent clss
+        {
+            //set variables to private
+            private string name;
+            private int id;
+            public int userid;
+            public int Id { get; set; }
+            //{
+            // get
+            //   return id;
+            // }
+            // set
+            // {
+            //    id = value;
+            // }
+            //}
+            public string Name { get; set; }
+
+        }
+        public class employee : user // : is use for inheritence Employee is inheriting the user class for geting and setting}
+        {
+            private string designation;
+            public string Designation { get; set; }
+            public static void main(string[] args)
+            {
+                user usr = new user();
+                usr.Id = 10;
+                usr.Name = "Anita";
+
+                employee e1 = new employee();
+                // access curent class member
+                e1.designation = "Professor";
+                // acces base class member
+                e1.Id = 29;
+                e1.Name = "Prashant";
+                Console.WriteLine(" Employee's Name is :" + e1.Name);
+                Console.WriteLine("Employee's Id is ;" + e1.Id);
+                Console.WriteLine("Employee's Role Id is :" + e1.Designation);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+            
+        
---
+++ Practice1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Logics1;
+using Xceed.Wpf.Toolkit;
+
+namespace PrashantConsoleApp1
+{
+    class Practice1
+    {
+        public static void Main(string[] args)
+            
+        {
+            Add obj = new Add();
+            obj.Addition();
+            DataTypes1 obj1 = new DataTypes1();
+            obj1.DTImplementation();
+            Logics1.Calculator obj2 = new Logics1.Calculator();
+            obj2.Addition();
+            obj2.Subtraction();
+            Console.ReadKey();
+        }
+        class Add
+        {
+            public void Addition()
+            {
+
+                int a = 50;
+                int b = 25;
+                int res = a + b;
+                Console.WriteLine("The addition of a and b is " + res);
+
+            }
+            
+
+           
+
+            
+        }
+
+
+    }
+}
