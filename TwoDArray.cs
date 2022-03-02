756bcaf26f8dd3e500d83c2121dbf498c290586c
PrashantNaktode <100767502+PrashantNaktode@users.noreply.github.com>
2022-03-02 22:25:02 +0530

    Add files via upload

---
+++ TwoDArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logics
+{
+    class LogicsTwoDArray
+    {
+        void TwoDimensionalArray()
+        {
+            int[,] a = new int[5, 2]
+            {
+            { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 6 }, { 4, 8 }
+            };
+            int i, j;
+            for (i = 0; i < 5; i++ )
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("a[{0},{1}] = {2}", i, j, a[i, j]);
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }   
+      
+}
+
