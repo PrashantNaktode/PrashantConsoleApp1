eac240e1f2b3b9a7bc234ba239853144421039ea
PrashantNaktode <100767502+PrashantNaktode@users.noreply.github.com>
2022-03-02 22:22:03 +0530

    Add files via upload

---
+++ DataTypes1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logics1
+{
+   public class DataTypes1
+   {
+        public void DTImplementation()
+        {
+            int intA;          //Variable Declaration
+            intA = 10;
+            //float b = 100.99;
+            //variable Initialisation
+            float floatB = 3.33434343434f;
+            double doubleC = 45.5454545454542424;      //Litear1
+            bool boolD = true;             //Local Variable
+            char charE = 'C';
+            decimal decimalF = 23;
+
+            Console.WriteLine(intA);
+            Console.WriteLine(floatB);
+            Console.WriteLine(doubleC);
+            Console.WriteLine(boolD);
+            Console.WriteLine(charE);
+            Console.WriteLine(decimalF);
+            //Console.WriteLine(TypeA);
+            //Console.ReadKey();
+
+        }
+    }
+}
